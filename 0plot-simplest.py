df9b8a687a597a003e69053e340fd8fa4381f974
jrgpulido <jrgpulido@maaMba.local>
2022-03-31 22:23:43 -0600

    plotting file

---
+++ 0plot-simplest.py
@@ -0,0 +1,15 @@
+#
+# simplest-
+#
+
+import matplotlib.pyplot as plt
+
+plt.plot(range(10))
+plt.show()
+
+
+
+plt.savefig('some_plot.png')
+plt.savefig('some_plot.pdf')
+plt.savefig('some_plot.jpg')
+plt.savefig('some_plot.eps')
